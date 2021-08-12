02cf46457473be148c9768bce4821e96e629c67b
Serge R <747500@gmail.com>
2021-08-13 00:42:47 +0300

    fixes d.ts

--- dist/petrovich.d.ts
+++ dist/petrovich.d.ts
@@ -6,12 +6,12 @@ export type PetrovichPerson = {
 };
 
 export enum PetrovichCase {
-	'nominative' = 'nominative'; // - именительный (кто? что?)
-	'genitive' = 'genitive' // - родительный (кого? чего?)
-	'dative' = 'dative' //  - дательный (кому? чему?)
-	'accusative' = 'accusative' //  - винительный (кого? что?)
-	'ablative' = 'ablative' //  - творительный (кем? чем?)
-	'prepositional' = 'prepositional' //  - предложный (о ком? о чем?)
+	'nominative' = 'nominative', // - именительный (кто? что?)
+	'genitive' = 'genitive', // - родительный (кого? чего?)
+	'dative' = 'dative', //  - дательный (кому? чему?)
+	'accusative' = 'accusative', //  - винительный (кого? что?)
+	'ablative' = 'ablative', //  - творительный (кем? чем?)
+	'prepositional' = 'prepositional', //  - предложный (о ком? о чем?)
 };
 
-export function petrovich(person: PetrovichPerson, case: PetrovichCase);
+export function petrovich(person: PetrovichPerson, case: PetrovichCase): PetrovichPerson;
